055cbcf3fb3d16997383273724c183cf8c5bb929
Akshat303 <sri303akshat@gmail.com>
2023-12-28 16:23:52 +0530

    update contact info

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //javascript for navigation bar effects on scroll
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
-  header.classList.toggle('sticky', window.scrollY > 0);
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 //javascript for responsive navigation sidebar menu
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
-const navigationItems = document.querySelectorAll(".navigation a")
+const navigationItems = document.querySelectorAll(".navigation a");
 
-menuBtn.addEventListener("click",  () => {
+menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   navigation.classList.toggle("active");
 });
@@ -24,7 +24,7 @@ navigationItems.forEach((navigationItem) => {
 //javascript for scroll to top button
 const scrollBtn = document.querySelector(".scrollToTop-btn");
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
   scrollBtn.classList.toggle("active", window.scrollY > 500);
 });
 
@@ -37,16 +37,44 @@ scrollBtn.addEventListener("click", () => {
 //javascript for reveal website elements on scroll
 window.addEventListener("scroll", reveal);
 
-function reveal(){
+function reveal() {
   var reveals = document.querySelectorAll(".reveal");
 
-  for(var i = 0; i < reveals.length; i++){
+  for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var revealTop = reveals[i].getBoundingClientRect().top;
     var revealPoint = 50;
 
-    if(revealTop < windowHeight - revealPoint){
+    if (revealTop < windowHeight - revealPoint) {
       reveals[i].classList.add("active");
     }
   }
+}
+
+function sendToWhatsapp() {
+  let number = "+919914805645";
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let message = document.getElementById("message").value;
+
+  var url =
+    "https://wa.me/" +
+    number +
+    "?text=" +
+    "Name : " +
+    name +
+    "%0a" +
+    "Mobile no. : " +
+    phone +
+    "%0a" +
+    "Email : " +
+    email +
+    "%0a" +
+    "Message : " +
+    message +
+    "%0a%0a";
+
+  window.open(url, "_blank").focus();
 }
